4a960834e52f17c80d9ea5f4210bcf2961d4ce24
najaa-22 <naja59613@gmail.com>
2025-09-15 15:34:30 +0530

    Add files via upload

---
+++ days.py
@@ -0,0 +1,18 @@
+a=int(input("enter a number:"))
+if a==1:
+    print(a[1])
+elif a==2:
+    print(a[2])
+elif a==3:
+    print(a[3])
+elif a==4:
+    print(a[4])
+elif a==5:
+     print(a[5])
+elif a==6:
+    print(a[6])
+else:
+    print("enter 1-7")
+     
+    
+    
---
+++ list,sum,fact.py
@@ -0,0 +1,16 @@
+"""list=[25,1,-3,4,-2,30]
+for i in list:
+    if i>0:
+        print(i)"""
+
+        
+"""total=0
+for i in range(1,11):
+    total+=i
+print("sum=",total)"""
+
+fact=1
+num=int(input("enter a number:"))
+for i in range(1,num+1):
+ fact*=i
+print("factorial=",fact)
