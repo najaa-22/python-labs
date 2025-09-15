dda027caa7d4af0386fd6af88584e31b2b38aa5d
najaa-22 <naja59613@gmail.com>
2025-09-15 16:07:13 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,8 @@
+num=int(input("enter an integer:"))
+if num > 0:
+    print("the number is positive")
+elif num<0:
+     print("the number is negative")
+else:
+    print("the number is zero")
+    
